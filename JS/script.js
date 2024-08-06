c03d9a47f5c35e8a00039bad0384124c982f2640
Phillip McKenzie <pmckenzie6387@gmail.com>
2024-08-06 11:49:48 -0400

    finished random mine generator

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,12 @@
-  /*----- constants -----*/
-  const BOARD_ROWS = 10;
-  const BOARD_COLS = 10;
-  let nets = [];
+/*----- constants -----*/
+const BOARD_ROWS = 10;
+const BOARD_COLS = 10;
 
-  /*----- state variables -----*/
-  let winner;
-  let looser;
-  // let cellIsNet;
-  let cellIsFlagged;
-  let board;
-  let netCount = 0;
+
+/*----- state variables -----*/
+let winner;
+let board;
+let cellEls;
 
 
 /*----- cached elements  -----*/
@@ -28,20 +25,19 @@ deadShrimpBtmEl.addEventListener('click', () => {
 
 /*----- functions -----*/
 init();
-
-function cellIsNet() {
-    while (netCount <= 9) {
-        const randomNet = Math.floor(Math.random() * 100) + 1;
-        nets.push(randomNet);
-        netCount += 1;
-        
-    };
-};
-// if the index inside the nets array is true, we want to assign in the board array isNet true ?  true ? true : false\
-// if () 
+ 
 function init() {
-    cellIsNet()
     board = [];
+    generateCellsInBoard();
+    cellEls = document.querySelectorAll('.cell');
+    setNets();
+    // computeAdjacentNetCounts()
+    winner = null;
+    // console.log(board)
+    render();
+}
+
+function generateCellsInBoard() {
     for (let rowIdx = 0; rowIdx < BOARD_ROWS; rowIdx++) {
         board[rowIdx] = [];
         for (let colIdx = 0; colIdx < BOARD_COLS; colIdx++) {
@@ -49,12 +45,13 @@ function init() {
                 isRevealed: false,
                 isFlagged: false,
                 isNet: false,// Randomly plant later
-                adjNetCount: null, // Compute after mines are determined
+                adjNetCount: 0, // Compute after mines are determined
                 rowIdx,  // shorthand property syntax
                 colIdx
             });
             const cell = document.createElement('div');
             cell.className = 'cell';
+            cell.id = `${rowIdx}-${colIdx}`;
             container.appendChild(cell);
             cell.addEventListener('click', () => {
                 cell.style.borderStyle = 'none'
@@ -64,28 +61,32 @@ function init() {
                 cell.innerText = '🛟'
             });
         }
-    };  
-    nets.forEach(() => {
+    }  
+}
 
-    });
-    console.log(board)
-    render();
-};
+function setNets() {
+    const NET_PCT = .2;
+    let netsToPlace = Math.round(BOARD_ROWS * BOARD_COLS * NET_PCT);
+    while (netsToPlace > 0) {
+        let rndRowIdx = Math.floor(Math.random() * BOARD_ROWS);
+        let rndColIdx = Math.floor(Math.random() * BOARD_COLS);
+        if (!board[rndRowIdx][rndColIdx].isNet) {
+            board[rndRowIdx][rndColIdx].isNet = true;
+            netsToPlace--;
+        } 
+    }
+}
+
 function render() {
     
-};
+}
 
-const cells = document.querySelectorAll('.cell')
-
-// cells.forEach((cell) => {
-//     cell.id 
-// })
-function resetBoard () {
+// function resetBoard () {
     
-};
+// };
 
 
-
-
+// clicking a cell with a net ends the game reveal all cells then call render 
+// link reset button to init 
 // TODO: Randomly plant mines
 // TODO: After planting mines, iterate over cells and compute adjMineCount
